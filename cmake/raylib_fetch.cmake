a0f3f3887dd98b2769b68501293228ca2e608fc7
Michaela Adams <30957743+MichJadams@users.noreply.github.com>
2025-06-30 12:35:25 -0400

    Update raylib_fetch.cmake

--- cmake/raylib_fetch.cmake
+++ cmake/raylib_fetch.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
 raylib
-GIT_REPOSITORY https://github.com:raysan5/raylib.git
+GIT_REPOSITORY https://github.com/raysan5/raylib.git
 GIT_TAG master)
 
 FetchContent_MakeAvailable(raylib) 
