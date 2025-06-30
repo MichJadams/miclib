72b994a12c4fd23e7bbfabc10317b393fa4e9ec7
Michaela Adams <mjadams@fastmail.com>
2025-06-30 12:27:18 -0400

    SSH to HTTPS
    Change the fit repository for raylib to the https link and not the ssh
    link because I was getting a permissions error.

--- cmake/raylib_fetch.cmake
+++ cmake/raylib_fetch.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
 raylib
-GIT_REPOSITORY git@github.com:raysan5/raylib.git
+GIT_REPOSITORY https://git@github.com:raysan5/raylib.git
 GIT_TAG master)
 
 FetchContent_MakeAvailable(raylib) 
